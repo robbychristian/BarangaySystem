442119c72c330fb56024e1147167a54b931498d9
robbychristian <61650887+robbychristian@users.noreply.github.com>
2023-11-09 11:07:10 +0800

    Update api.js

--- resources/js/config/api.js
+++ resources/js/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const DEV_URL = "http://127.0.0.1:8000/api/";
+export const PROD_URL = "https://brgyugongpasigcity.com/api/";
 
 export const api = axios.create({
     baseURL: DEV_URL,
