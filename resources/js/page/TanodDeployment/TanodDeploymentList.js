d1ecd8234cbb61bb28a5d6bdb7ffa5b8d203fe97
robbychristian <61650887+robbychristian@users.noreply.github.com>
2023-12-13 09:58:41 +0800

    backup

---
+++ resources/js/page/TanodDeployment/TanodDeploymentList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const TanodDeploymentList = () => {
+    return (
+        <div className='px-10 py-4'>
+            <div>Tanod Deployment List</div>
+        </div>
+    );
+}
+
+export default TanodDeploymentList;
